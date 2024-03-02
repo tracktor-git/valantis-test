462faed9386cb8dea5374a94ef7d001be5ba00e6
Tracktor <tracktor@bk.ru>
2024-03-02 12:28:04 +0300

    Add GoToPage in Paginator

--- src/Components/Paginator/Paginator.jsx
+++ src/Components/Paginator/Paginator.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './Paginator.module.css';
 
 const ChangePageButton = ({ disabled, onClick, direction }) => {
@@ -15,6 +16,31 @@ const ChangePageButton = ({ disabled, onClick, direction }) => {
     >
       {map[direction]}
     </button>
+  );
+};
+
+const GoToPage = ({ isLoading, maxValue, onSubmit }) => {
+  const [value, setValue] = React.useState();
+
+  const handleChange = (event) => {
+    setValue(Number(event.target.value));
+  };
+
+  const handleGoToPage = (event) => {
+    event.preventDefault();
+    onSubmit(value);
+  };
+
+  return (
+    <div className={styles.goToPage}>
+      <form onSubmit={handleGoToPage}>
+        <label htmlFor="pageNumber">
+          <span>На страницу:</span>
+          <input id="pageNumber" required type="number" min={1} max={maxValue} onChange={handleChange} disabled={isLoading} />
+          <button className={styles.go} type="submit" disabled={isLoading}>Go!</button>
+        </label>
+      </form>
+    </div>
   );
 };
 
@@ -36,14 +62,23 @@ const Paginator = (props) => {
     ? `Страница ${pageNumber} из ${pagesCount}`
     : 'Страница ... из ...';
 
+  const handleGoToPage = (value) => {
+    if (value !== pageNumber) {
+      changePage(value);
+    }
+  };
+
   return (
-    <div className={styles.paginatorWrapper}>
-      <ChangePageButton direction="previous" onClick={decreasePage} disabled={isPreviousDisabled} />
-      <div className={styles.pageNumber}>
-        <span>{pageText}</span>
+    <>
+      <div className={styles.paginatorWrapper}>
+        <ChangePageButton direction="previous" onClick={decreasePage} disabled={isPreviousDisabled} />
+        <div className={styles.pageNumber}>
+          <span>{pageText}</span>
+        </div>
+        <ChangePageButton direction="next" onClick={increasePage} disabled={isNextDisabled} />
       </div>
-      <ChangePageButton direction="next" onClick={increasePage} disabled={isNextDisabled} />
-    </div>
+      <GoToPage isLoading={isLoading} maxValue={pagesCount} onSubmit={handleGoToPage} />
+    </>
   );
 };
 
